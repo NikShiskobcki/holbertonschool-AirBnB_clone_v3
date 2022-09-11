e40e6248aef78e063760c6486a9b0f61c03fe521
EugeniaMatto <eugematto26@gmail.com>
2022-09-10 17:54:14 -0700

    task 12 fix

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -18,7 +18,7 @@ def retrieve_reviews(place_id):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    reviews = [review.to_dict() for review in place.reviews()]
+    reviews = [review.to_dict() for review in place.reviews]
     return jsonify(reviews)
 
 
